894c8d09dfd066e5717d576ffb4f98e9b891d980
anannya mistry <anannyamistry@anannyas-MacBook-Air.local>
2023-07-06 15:41:44 +0530

    creatd the crypto app

--- crypto-hunter/src/App.js
+++ crypto-hunter/src/App.js
@@ -1,7 +1,30 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepgae";
+import Coin from "./pages/Coin";
+import { makeStyles } from "@material-ui/styles";
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#000000",
+    color: "#fff",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
-  return <div className="App">hello world</div>;
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" Component={Homepage} exact />
+          <Route path="/coins/:id" Component={Coin} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ crypto-hunter/src/components/Header.js
@@ -0,0 +1,32 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+const Header = () => {
+  return (
+    <AppBar color="primary" position="static">
+      <Container>
+        <Toolbar>
+          <Typography>Crypto Hunter</Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40,
+              marginLeft: 15,
+            }}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"INR"}>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
